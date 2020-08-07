ec288d7b56beda8060dba85df532b1ca6102f257
Luis Felipe <felipeluis610@gmail.com>
2020-08-07 16:57:10 -0300

    Criando o servidor

--- src/server.js
+++ src/server.js
@@ -1,85 +1,10 @@
-//Dates
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "98654575",
-        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Daniele Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "98654575",
-        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [ 
-        "Artes",
-        "Biologia",
-        "Ciências",
-        "Educação física",
-        "Física",
-        "Geografia",
-        "História",
-        "Matemática",
-        "Português",
-        "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    const arrayPosition = +subjectNumber - 1
-    return subjects[arrayPosition]
-}
-
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html",{proffys,filters,subjects,weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const insNotEmpty = Object.keys(data).length > 0
-    if(insNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html",{subjects,weekdays})
-}
-
 //Server
 const express = require('express')
 const server = express()
+const {pageLanding,
+       pageStudy,
+       pageGiveClasses,
+       saveClasses} = require('./pages')
 
 //to set up nunjucks(template engine)
 const nunjucks = require('nunjucks')
@@ -89,11 +14,14 @@ nunjucks.configure('src/views', {
 }) 
 
 server
+    //Receive the data do req.body()
+    .use(express.urlencoded({extended: true}))
     //Configuring static files(Css,images,scripts)
     .use(express.static("public"))
     //Routes of aplication
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
 
-    .listen(5500)
+    .listen(5500);
